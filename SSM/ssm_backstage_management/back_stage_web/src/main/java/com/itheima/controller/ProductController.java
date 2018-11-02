2b93a8d34b7149e565d5f49ae1db8159216fbf7e
tongqiao <joetonghao@126.com>
2018-11-02 10:52:21 +0800

    修正一些配置

---
+++ SSM/ssm_backstage_management/back_stage_web/src/main/java/com/itheima/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.itheima.controller;
+
+import com.itheima.domain.Product;
+import com.itheima.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private IProductService productService;
+
+    @RequestMapping("/findAll.do")
+    public String findAll(Model model){
+
+        List<Product> products = productService.findAll();
+        model.addAttribute("",products);
+        return "";
+    }
+}

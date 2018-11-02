6f70dd575b3454d6f967f8f10879568ca1509fc8
tongqiao <joetonghao@126.com>
2018-11-02 19:22:56 +0800

    产品管理页面编写

--- SSM/ssm_backstage_management/back_stage_web/src/main/java/com/itheima/controller/ProductController.java
+++ SSM/ssm_backstage_management/back_stage_web/src/main/java/com/itheima/controller/ProductController.java
@@ -22,6 +22,6 @@ public class ProductController {
         List<Product> products = productService.findAll();
         model.addAttribute("productList",products);
         System.out.println(products);
-        return "product-list";
+        return "productList";
     }
 }
